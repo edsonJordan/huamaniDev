c6561c9bcbcc2bbbb5d3d882ecc4ab9f118f3683
Edson Jordan <edson_4555@hotmail.com>
2021-05-18 13:27:42 -0500

    iniciando proyecto de portafolio y blog

---
+++ routes/web.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Aqui escribira su codigo un futuro maldito de laravel
+|--------------------------------------------------------------------------
+|
+| Eres un campeón ¡¡¡
+| No te rindas sigue solo falta poco¡¡¡¡
+| Quieres ser un programador o no ?? sigue escribiendo codigo carajo ¡¡¡
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
